04071767d4e7614ad164f4d1c77ccfe8d666245c
jai-riley <145622900+jai-riley@users.noreply.github.com>
2024-11-07 02:01:07 -0700

    Modified quick recap 3 to follow pattern of quick recap

--- HackerRank/AI/Correlation and Regression Lines - A Quick Recap #2.py
+++ HackerRank/AI/Correlation and Regression Lines - A Quick Recap #2.py
@@ -3,17 +3,15 @@ Here are the test scores of 10 students in physics and history:
 
 Physics Scores  15  12  8   8   7   7   7   6   5   3
 History Scores  10  25  17  11  13  17  20  13  9   15
-When a student scores 10 in Physics, what is his probable score in History?
-Compute the answer correct to one decimal place.
+Compute the slope of the line of regression obtained while treating
+Physics as the independent variable. Compute the answer correct to
+three decimal places.
 """
+
 p = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]
 h = [10, 25, 17, 11, 13, 17, 20, 13, 9, 15]
-X = 10
 mean_p = sum(p) / len(p)
 mean_h = sum(h) / len(h)
 numerator = sum([(p[x]-mean_p)*(h[x]-mean_h) for x in range(len(p))])
 squared_sum_p = sum([(p[x]-mean_p)**2 for x in range(len(p))])
-w1 = numerator/squared_sum_p
-w0 = mean_h - w1 * mean_p
-y = w0 + w1 * X
-print(y)
+print(f"{(numerator/squared_sum_p):.3f}")
